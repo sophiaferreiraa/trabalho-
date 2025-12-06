04c9f816315de064e886d37c5e1a8ce212ea5642
sophiaferreiraa <137004738+sophiaferreiraa@users.noreply.github.com>
2025-12-06 18:26:40 -0300

    Delete TP-Web-main directory

--- TP-Web-main/admin_vendas.php
+++
@@ -1,7 +0,0 @@
-<?php
-// admin_vendas.php (Na raiz)
-require_once 'controllers/VendaController.php';
-
-$controller = new VendaController();
-$controller->relatorioVendas();
-?>
--- TP-Web-main/ver_compras.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ver_compras.php (Na raiz)
-require_once 'controllers/VendaController.php';
-
-$controller = new VendaController();
-$controller->minhasCompras();
-?>
