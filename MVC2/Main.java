fbee924f2b0fd2428d0392049f0db13b4adc17b6
sophiaferreiraa <137004738+sophiaferreiraa@users.noreply.github.com>
2025-12-11 16:23:40 -0300

    Delete MVC2 directory

--- MVC2/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Model model = new Model();
-        View view = new View();
-        new Controller(model, view);
-    }
-}
