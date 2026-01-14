6d03b25735ee468fa9fc9cba588d9748055cdcc2
sophiaferreiraa <137004738+sophiaferreiraa@users.noreply.github.com>
2026-01-14 18:32:14 -0300

    Delete BD_MCV_RMI_PROXY directory

--- BD_MCV_RMI_PROXY/cadastro_pessoa.sql
+++
@@ -1,9 +0,0 @@
--- 1. Criação do Banco de Dados (conforme string em Conexao.java)
-CREATE DATABASE IF NOT EXISTS cadastro_pessoa;
-USE cadastro_pessoa;
--- 2. Criação da Tabela (conforme queries em PessoaDAO.java)
-CREATE TABLE IF NOT EXISTS pessoa (
-    id INTEGER PRIMARY KEY,  -- para fazer id alto incremente : id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL, -- String nome em Pessoa.java
-    escola VARCHAR(255) NOT NULL -- String email em Pessoa.java
-);
